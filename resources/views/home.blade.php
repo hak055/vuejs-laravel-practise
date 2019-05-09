12266866b78b50958d47c07c61102a83e496bda5
hak055 <hak055-sahakyan@yandex.ru>
2019-05-09 21:10:26 +0300

    some chenges

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> Post</div>
                 <div class="pr-5"><strong>21k</strong> Followers</div>
-                <div class="pr5"><strong>23k</strong> Following</div>
+                <div class="pr-5"><strong>23k</strong> Following</div>
             </div>
         </div>
     </div>
@@ -19,6 +19,12 @@
         <div class="col-4"><img src="https://www.rhs.org.uk/getmedia/5f34b844-ec18-4f72-bc0c-ecbd7d10c03d/RHS-Chelsea-Flower-Show-2009_RHS_MAR0001167.jpg?width=520&height=346&ext=.jpg" width="100%"></div>
         <div class="col-4"><img src="https://www.rhs.org.uk/getmedia/b8feee1b-a131-48e3-a3fd-96cf6099faed/_RHS_PUB0024356.jpg?width=520&height=346&ext=.jpg" width="100%"></div>
         <div class="col-4"><img src="https://www.rhs.org.uk/getmedia/24bf0be7-7d38-4acc-b8f3-88f5809c9fed/RHS-Chelsea-Flower-Show-2009_RHS_MAR0001050-B.jpg?width=520&height=346&ext=.jpg" width="100%"></div>
+    </div>
+
+    <div class="row pt-4">
+        <div class="col-4"><img src="https://hips.hearstapps.com/clv.h-cdn.co/assets/17/11/1489688321-clx050109-091-1-4.jpg?crop=0.567xw:0.841xh;0.111xw,0.0725xh&resize=480:*" width="100%" height="350px"></div>
+        <div class="col-4"><img src="https://i.pinimg.com/originals/90/75/a2/9075a246b29204764d4493587e753cdf.jpg" width="100%" height="350px"></div>
+        <div class="col-4"><img src="https://i.pinimg.com/originals/b8/37/aa/b837aa6c473ab5b98358fa06dcabcf92.jpg" width="100%" height="350px"></div>
     </div>
 </div>
 @endsection
