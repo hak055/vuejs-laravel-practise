abb4f17da7a0ee2b897008d5acab13c72fc28e26
hak055 <hak055-sahakyan@yandex.ru>
2019-05-09 17:54:34 +0300

    lang, add username-никнайм and any chenges in views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://image.flaticon.com/icons/svg/61/61222.svg" width="100px" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>vuejs-laravel-practise</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> Post</div>
+                <div class="pr-5"><strong>21k</strong> Followers</div>
+                <div class="pr5"><strong>23k</strong> Following</div>
+            </div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4"><img src="https://www.rhs.org.uk/getmedia/5f34b844-ec18-4f72-bc0c-ecbd7d10c03d/RHS-Chelsea-Flower-Show-2009_RHS_MAR0001167.jpg?width=520&height=346&ext=.jpg" width="100%"></div>
+        <div class="col-4"><img src="https://www.rhs.org.uk/getmedia/b8feee1b-a131-48e3-a3fd-96cf6099faed/_RHS_PUB0024356.jpg?width=520&height=346&ext=.jpg" width="100%"></div>
+        <div class="col-4"><img src="https://www.rhs.org.uk/getmedia/24bf0be7-7d38-4acc-b8f3-88f5809c9fed/RHS-Chelsea-Flower-Show-2009_RHS_MAR0001050-B.jpg?width=520&height=346&ext=.jpg" width="100%"></div>
+    </div>
+</div>
+@endsection
