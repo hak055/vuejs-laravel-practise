63a63daed8ee5ac2a41cec0891854549984fb674
hak055 <hak055-sahakyan@yandex.ru>
2019-05-11 18:50:29 +0300

    chenge warning in post

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://image.flaticon.com/icons/svg/61/61222.svg" width="100px" class="rounded-circle">
+            <img src="https://image.flaticon.com/icons/svg/61/61222.svg" width="80px" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between">
@@ -12,7 +12,7 @@
                 <a href="/p/create">New Post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>153</strong> Post</div>
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> Post</div>
                 <div class="pr-5"><strong>21k</strong> Followers</div>
                 <div class="pr-5"><strong>23k</strong> Following</div>
             </div>
@@ -21,13 +21,13 @@
             <div><a href="#">{{$user->profile->url ?? "N/A"}}</a> </div>
         </div>
     </div>
-    <div class="row pt-4">
+    <div class="row p-5">
 
         @foreach($user->posts as $post)
-            <div class="col-4">
-                {{$post['caption']}}
-                {{$post['image']}}
-                <img src="" width="100%">
+            <div class="col-4 p-2">
+                
+                
+                <img src="/storage/{{$post->image}}" class="w-100">
                 
             </div>
         @endforeach
